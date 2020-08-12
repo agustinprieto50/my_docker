c1504e3109d95c6aaf51a6b27357fcd9e7ddf3de
Agustin <agustinprieto50@gmail.com>
2020-08-12 14:10:23 -0300

    PRODUCT COMMIT

---
+++ product/productoServices.py
@@ -0,0 +1,26 @@
+from repositorios import Repositorios
+
+
+class ProductoService():
+
+    def add_producto(self, producto):
+        lastKey = -1
+        for key in Repositorios.productosList:
+            lastKey = key
+        new = int(lastKey) + 1
+        Repositorios.productosList[new] = producto.__dict__
+        return new
+
+    def delete_producto(self, num):
+        if num not in Repositorios.productosList:
+            raise ValueError("El producto no existe")
+        else:
+            del Repositorios.productosList[num]
+
+    def get_productosList(self):
+        return Repositorios.productosList
+
+    def update_producto(self, key, producto):
+        if key not in Repositorios.productosList:
+            raise ValueError("El lproducto no existe")
+        Repositorios.productosList.update({key: producto.__dict__})
