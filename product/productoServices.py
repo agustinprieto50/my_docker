85554112e5b0496f3c00c55f262a017791925133
Agustin <agustinprieto50@gmail.com>
2020-08-19 19:53:23 -0300

    Asertion Sort Product

--- product/productoServices.py
+++ product/productoServices.py
@@ -24,3 +24,38 @@ class ProductoService():
         if key not in Repositorios.productosList:
             raise ValueError("El producto no existe")
         Repositorios.productosList.update({key: producto.__dict__})
+
+    # def insertion_sort_precio(self, lista, orden):
+    #     if orden == str('ascendente'):
+    # # We start from 1 since the first element is trivially sorted
+    #         for index in range(1, len(lista)):
+    #             currentValue = lista[index]["_precio"]
+    #             currentPosition = index
+
+    #             while currentPosition > 0 and lista[currentPosition - 1]
+    #                   > currentValue:
+    #                 lista[currentPosition] = lista[currentPosition - 1]
+    #                 currentPosition = currentPosition - 1
+
+    def insertion_sort_precio(self, lista, tipo_orden):
+        if tipo_orden == str("ascendente"):
+            largo_indice = range(1, len(lista))
+            for i in largo_indice:
+                sort = lista[i]
+
+                while i > 0 and lista[i-1]["_precio"] > sort["_precio"]:
+                    lista[i], lista[i-1] = lista[i-1], lista[i]
+                    i = i-1
+
+            return lista
+
+        if tipo_orden == str("descendente"):
+            largo_indice = range(0, len(lista))
+            for i in largo_indice:
+                sort = lista[i]
+
+                while i > 0 and lista[i-1]["_precio"] < sort["_precio"]:
+                    lista[i], lista[i-1] = lista[i-1], lista[i]
+                    i = i-1
+
+            return lista
